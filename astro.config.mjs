89236a39278c20cf7cd31c24a1c586025d6a86ec
di7a <76627014+dif7a@users.noreply.github.com>
2025-12-28 00:46:49 +0530

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  site: 'https://dif7a.github.io',
+  base: '/portfolio',
+})
---
+++ config.ts
@@ -0,0 +1,80 @@
+export const siteConfig = {
+  name: "Diya Gandhi",
+  title: "front-end.web{designer}",
+  description: "Portfolio website of Diya Gandhi",
+  accentColor: "#7400b8",
+  social: {
+    email: "diyagandhi498@gmail.com",
+    linkedin: "https://www.linkedin.com/in/diya-gandhi-023078294",
+    github: "https://github.com/dif7a",
+  },
+  aboutMe:
+    "Dedicated to turning ideas into creative solutions with focus on activities that sharpen my craft and values that shape how I work. I describe specific hobbies that influence my design approach and the passions that steer my professional choices. Specialize in creating seamless and intuitive user experiences.By prioritizing performance, accessibility, and responsiveness, As I strive to deliver experiences that not only engage users but also drive tangible results.",
+  skills: ["Javascript","ES6","HTML5", "CSS3", "SASS/SCSS", "Figma", "Adobe XD","GIT/GITHUB"],
+  projects: [
+    {
+      name: "SheCodes Contest Project: Travel Web-app",
+      description:
+        "This user-friendly platform allows users to explore diverse destinations, discover hidden gems with seamless planning experience.",
+      link: "https://dif7a.github.io/Travel_app/",
+    },
+    { name: "Weather Web-app",
+      description:
+        " Provides current weather information and forecasts for locations worldwide, enhancing user accessibility to weather data.",
+      link: "https://dif7a.github.io/weather_app/",
+    },
+    {
+      name: "Todo List Web-app",
+      description:
+        " Developed a responsive web application Aimed at improving for personal productivity, managing tasks and to-do lists, using CRUD operations.",
+      link: "https://dif7a.github.io/Todo_list/",
+    },
+    {
+      name: "Virtual Mood Tracker Application",
+      description:
+        "Designed and implemented to help users monitor and analyze their mood patterns over time, promoting mental wellness and self-awareness."
+    },
+    {
+      name: "Deco's Hackathon Project: Indian musical instruments Web-app",
+      description:
+        "Created a comprehensive web platform dedicated to showcasing traditional, promoting cultural heritage and musical education."
+    },
+  ],
+  experience: [
+    {
+      company: "Slash Mark IT Startup",
+      title: "Full Stack Web Developer Intern",
+      bullets: [
+        "Developed responsive and user-friendly web interfaces using HTML5, CSS3, and JavaScript.",
+        "Implemented responsive design strategies and front-end features to improve user experience.",
+        "Created mockups for the website layout and design",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "Parul University",
+      degree: "Bachelor of Technology in Computer Science and Engineering",
+      achievements: [
+        "2020-2024"
+      ],
+    },
+  ],
+  certifications: [
+    {
+      school: "DesignCourse",
+      degree: "UI/UX Crash Course",
+      achievements: [],
+    },
+    {
+      school: "FreeCodeCamp",
+      degree: "Legacy JavaScript Algorithms and Data Structures",
+      achievements: [],
+    },
+    {
+      school: "Udemy",
+      degree: "Modern HTML & CSS (Including SASS)",
+      achievements: [],
+    },
+  ],
+};
